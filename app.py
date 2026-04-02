faa900ed4c5d3497f21cbc7a2af680c608e175ef
hwinhwork-dot <hwinh.work@gmail.com>
2026-04-03 00:40:56 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Hồ sơ năng lực của Nguyễn Hoàng Minh (hwinh)")
+st.write("Chào mừng đến với không gian số của tôi!")
+st.write("Dự án AI và Đổi mới sáng tạo sẽ được cập nhật tại đây.")
